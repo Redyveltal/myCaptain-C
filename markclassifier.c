269724cd8a7c9f19918de930479ce1f279aaeb58
Adithya B Panicker <83650397+Redyveltal@users.noreply.github.com>
2021-10-16 21:22:56 +0400

    Mark Classification
    Task 3 for C Course

---
+++ markclassifier.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+	printf("Enter your marks: ");
+	int a = 0;
+	scanf("%d", &a);
+	printf("Your entered mark is : %d \n", a);
+	if(a>85)
+	{
+		printf("You have obtained Grade A");
+	}
+	else if(a>70)
+	{
+		printf("You have obtained Grade B");
+	}
+	else if(a>55)
+	{
+     	printf("You have obtained Grade C");
+	}
+	else if(a>40)
+	{
+     	printf("You have obtained Grade D");
+	}
+	else
+	{
+		printf("You have obtained Grade F");
+	}
+}
