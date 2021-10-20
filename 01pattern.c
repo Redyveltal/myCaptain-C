9a42743b2f8128cfd6c00c630e27dfa9a4cd523c
Adithya B Panicker <83650397+Redyveltal@users.noreply.github.com>
2021-10-20 19:13:58 +0400

    01 Pattern

--- 01pattern.c
+++ 01pattern.c
@@ -2,8 +2,11 @@
 main()
 {
 	int i,j,n;
+	//Get input of rows from user
 	printf("Enter the number of rows you want: ");
 	scanf("%d", &n);
+	
+	//For loop 
 	for(i=0; i<n; ++i)
 	{
 		for(j=0; j<=i; ++j)
